780cd0d39118de59b8d3f9a735cb5e6c7043da79
srikanth <aryansri910@gmail.com>
2020-06-15 09:08:50 +0530

    added

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-     
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          srikn
         </a>
       </header>
     </div>
